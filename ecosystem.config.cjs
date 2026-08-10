22125322dd8074456ce136490abdf376788558de
Zhihao Liu <liuzhihao109@foxmail.com>
2026-08-10 13:46:44 +0800

    chore: change port to 4173 in ecosystem config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,19 +4,19 @@ module.exports = {
       name: "wyqd-app",
       cwd: __dirname,
       script: "scripts/serve-static.mjs",
-      args: "out 3000",
+      args: "out 4173",
       interpreter: "node",
       env: {
         NODE_ENV: "production",
         HOST: "0.0.0.0",
-        PORT: "3000",
+        PORT: "4173",
       },
     },
     {
       name: "wyqd-app-dev",
       cwd: __dirname,
       script: "node_modules/next/dist/bin/next",
-      args: "dev --hostname 0.0.0.0 --port 3000",
+      args: "dev --hostname 0.0.0.0 --port 4173",
       interpreter: "node",
       env: {
         NODE_ENV: "development",
